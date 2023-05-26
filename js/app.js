e70a4be8aa52993e45aa6c2766fc680118140d53
Tom Boudard <113201740+TomBoudard@users.noreply.github.com>
2023-05-26 03:13:23 +0200

    code : New logo and parallax upgrade

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0, yValue = 0;
+let rotateDegree = 0;
+
+function update(cursorXPosition){
+    parallax_el.forEach((el) => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        let speedz = el.dataset.speedz;
+        let isInLeft = el.dataset.left;
+        let rotateSpeed = el.dataset.rotation;
+
+        let zValue = (cursorXPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 1;
+
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) rotateY(${rotateDegree * rotateSpeed}deg) translateY(calc(-50% + ${-yValue * speedy}px)) perspective(2300px) translateZ(${-zValue * speedz}px)`;
+    });
+}
+
+update(0);
 
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2 ;
     yValue = e.clientY - window.innerHeight / 2;
 
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
 
-    parallax_el.forEach((el) => {
-        let speedx = el.dataset.speedx;
-        let speedy = el.dataset.speedy;
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${-yValue * speedy}px))`;
-    });
+    update(e.clientX);
+
+    
 });
